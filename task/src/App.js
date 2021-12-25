4ec2d50ac2883e69cc11c09c3500a5801eba0fb3
C Naveen Kumar <c.naveenkumar74@gmail.com>
2021-12-25 19:00:24 +0530

    Update App.js

--- task/src/App.js
+++ task/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const gitHubUrl = "https://reqres.in/api/users?page=1 ";
 function App() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getGitHubUserWithFetch();
+  }, []);
+
+  const getGitHubUserWithFetch = async () => {};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>user data</h1>
       </header>
+      <div className="user-container">
+        <h5 className="info-item">{userData.name}</h5>
+        <h5 className="info-item">{userData.location}</h5>
+        <h5 className="info-item">{userData.blog}</h5>
+        <h5 className="info-item">{userData.company}</h5>
+      </div>
     </div>
   );
 }
