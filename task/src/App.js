571296c5b26c61aaf5b26b374d3d68892e957c1e
C Naveen Kumar <c.naveenkumar74@gmail.com>
2021-12-31 08:42:55 +0530

    Completed!

--- task/src/App.js
+++ task/src/App.js
@@ -1,29 +1,42 @@
-import logo from "./logo.svg";
-import React, { useState, useEffect } from "react";
-import "./App.css";
-const gitHubUrl = "https://reqres.in/api/users?page=1 ";
-function App() {
-  const [userData, setUserData] = useState({});
+import React, { useState } from "react";
+import "./style.css";
+export default function App() {
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    getGitHubUserWithFetch();
-  }, []);
-
-  const getGitHubUserWithFetch = async () => {};
-
+  const loadUser = async () => {
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>user data</h1>
-      </header>
-      <div className="user-container">
-        <h5 className="info-item">{userData.name}</h5>
-        <h5 className="info-item">{userData.location}</h5>
-        <h5 className="info-item">{userData.blog}</h5>
-        <h5 className="info-item">{userData.company}</h5>
+    <div classNmae="jumbotron">
+      <div className="container">
+        <h1 className="fs-1">TASK 2</h1>
+        <button class="btn1 " onClick={loadUser}>
+          Get User
+        </button>
+      </div>
+      <div className="row">
+        {users.map(({ id, email, first_name, last_name, avatar }) => (
+          <div className="col-lg-4 col-md-4 col-sm-2">
+            <div className="card">
+              <img className="card-img-top" src={avatar} alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title" key={id}>
+                  ID: {id}
+                </h5>
+                <h5
+                  className="card-title"
+                  key={id}
+                >{`${first_name} ${last_name}`}</h5>
+                <p className="card-title" key={id}>
+                  {email}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
-export default App;
